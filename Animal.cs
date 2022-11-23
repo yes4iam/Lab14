ce452c83bbbefe8ffd7c3c0d57a600e646dc7884
Ilya Markov <imarkovkin@gmail.com>
2022-11-23 22:39:48 +0300

    Добавьте файлы проекта.

---
+++ Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            
+            Say();
+        }
+    }
+
+    class Dog : Animal
+    {
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name)
+            : base(name)
+        {
+           
+        }
+        public override void Say()
+        {
+            Console.WriteLine("wof");
+        }
+    }
+    class Cat : Animal
+    {
+        string name;        
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name)
+            : base(name)
+        {
+          
+        }
+        public override void Say()
+        {
+            Console.WriteLine("meow");
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите имя собаки");
+            Dog dog = new Dog("Шарик");
+            Console.WriteLine("Введите имя кота");
+            Cat cat = new Cat(Console.ReadLine());
+            Console.WriteLine(dog.ShowInfo());
+            Console.WriteLine(cat.ShowInfo());
+            Console.ReadKey();
+            
+        }
+    }
+}
